b1fa173d4889783c559139c50aab624604ce698e
dianavn95 <202100637@estudantes.ips.pt>
2026-03-16 09:19:12 +0000

    ignore n tá a funcionar
    já n sei

---
+++ PetLink/PetLink/Views/Shared/UserSideBar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetLink.Views.Shared
+{
+    public class UserSideBarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
