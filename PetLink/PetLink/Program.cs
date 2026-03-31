d99953fbb0428e137273d8743a6ec4e9136f024e
Bruna Rossa <202200603@estudantes.ips.pt>
2026-03-31 20:29:50 +0100

    Return Hashed ids

--- PetLink/PetLink/Program.cs
+++ PetLink/PetLink/Program.cs
@@ -4,8 +4,6 @@ using PetLink.Hubs;
 
 using Microsoft.AspNetCore.Authentication.Cookies;
 
-using HashidsNet;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -24,8 +22,6 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 builder.Services.AddSignalR();
 
-// Configura o gerador de Hashids
-builder.Services.AddSingleton<IHashids>(new Hashids("O_Teu_Segredo_PetLink_2026", minHashLength: 6));
 
 var app = builder.Build();
 
