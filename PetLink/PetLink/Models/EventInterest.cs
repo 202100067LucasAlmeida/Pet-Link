ad60e42d33ba1a51ee8ca025d5910eaf58ba3766
Rita Pereira <202200170@estudantes.ips.pt>
2026-06-22 18:31:51 +0100

    model EventInterest

---
+++ PetLink/PetLink/Models/EventInterest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetLink.Models
+{
+    public class EventInterest
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int EventId { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        public DateTime RegisteredAt { get; set; } = DateTime.Now;
+
+        public bool IsConfirmed { get; set; } = false; 
+
+        // Navigation properties
+        [ForeignKey("EventId")]
+        public virtual Event Event { get; set; }
+
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; }
+    }
+}
